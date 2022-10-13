aac252b0c283f12474f41749c41bbfc25895fdf0
Sahil Ahmed <56019538+sahilahmed0707@users.noreply.github.com>
2022-10-13 21:00:54 +0530

    Create Ackley.py

---
+++ GA/Ackley.py
@@ -0,0 +1,171 @@
+import numpy as np
+import random as rd
+import math
+
+
+# defining objective funtion (we will use this function as fitness as well and make it subject to minimization)
+def ackley(x):
+    sum = 0
+    sum -= 20 * (np.e ** (-0.2 * math.sqrt(0.5 * ((x[0]*x[0])+(x[1]*x[1])))))
+    sum += np.e + 20
+    sum -= np.e ** (0.5 * (np.cos(2*np.pi*x[0]) + np.cos(2*np.pi*x[1])))
+    return sum
+
+
+# initialize population and store fitness in first column and genes in second column
+def initialization(pop):
+    p = []
+    for b in range(0, pop):
+        x = []
+        for a in range(0, 2):
+            x.append(10*rd.uniform(0, 1) - 5)
+        r = ackley(x)
+        p.append([r, x])
+    return p
+
+
+# helper function to print only the fitness of a solution
+def print_pop(pop):
+    print("[", end="")
+    for x in pop:
+        print(x[0], end=", ")
+    print("]")
+
+
+# function to calculate average fitness of population
+def avfitness(p):
+    sum = 0.0
+    for x in p:
+        sum += x[0]
+    return sum/len(p)
+
+
+# formula to get probabality according to rank
+def rankprob(i, n):
+    s = 1.2
+    prob = ((2-s)/n) + (2*i*(s-1))/(n*(n-1))
+    return prob
+
+
+# selection - rank based selection
+def selection(pop, selected):
+    pop.sort(reverse=True)  # sort from worst to best
+
+    # calculation of selection probability for the entire population
+    prob = [rankprob(i, len(pop)) for i in range(0, len(pop))]
+    cumprob = []
+    sum = 0
+    for i in range(0, len(prob)):  # calculation of cumulative probability
+        sum += prob[i]
+        cumprob.append(sum)
+
+    mating_pool = []
+    for i in range(0, selected):
+        r = rd.uniform(0, 1)
+        j = 0
+        while (r > cumprob[j]):  # adding to mating pool using cumulative probability
+            j += 1
+        mating_pool.append(pop[j])
+    return mating_pool
+
+
+# whole arithmetic recombination with alpha 0.2
+# and with some recombination probablity for each gene,
+# making 2 children, choosing better one
+def recombination(a, b, rec_prob):
+    alpha = 0.2
+    alphainv = 1-alpha
+    child1 = [0 for i in range(0, 2)]
+    child2 = [0 for i in range(0, 2)]
+    for i in range(0, 2):
+        rand = rd.uniform(0, 1)
+        # either using recombination or copying the parents directly using recombination probablity
+        if(rand < rec_prob):
+            child1[i] = (alpha*a[i]) + (alphainv*b[i])
+            child2[i] = (alpha*a[i]) + (alphainv*b[i])
+        else:
+            child1[i] = a[i]
+            child2[i] = b[i]
+    # choosing and returning the better child
+    ras1 = ackley(child1)
+    ras2 = ackley(child2)
+    res = []
+    if(ras1 < ras2):
+        res = [ras1, child1]
+    else:
+        res = [ras2, child2]
+    return res
+
+
+# applying recombination to entire mating pool
+def crossover(mating_pool, rec_prob):
+    i = 0
+    children = []
+    while(i < len(mating_pool)):
+        children.append(recombination(
+            mating_pool[i][1], mating_pool[i+1][1], rec_prob))
+        i += 2
+    return children
+
+
+# applying mutation using Non-Uniform mutation with mutation probablity
+def mutation(children, mut_prob):
+    for x in children:
+        for g in x[1]:
+            r = rd.uniform(0, 1)
+            if(r < mut_prob):
+                mutation = rd.uniform(-50, 50)
+                g += mutation
+
+                # clamping the mutated gene within the search range
+                if(g > 5):
+                    g = 5.0
+                if(g < -5):
+                    g = -5.0
+
+        # recalculatng fitness
+        x[0] = ackley(x[1])
+
+    return children
+
+
+# replacing worst 50% by best of chidren and parents combined
+def replace_worst(pop, children):
+    pop_temp = pop + children
+    pop_temp.sort()
+    return pop_temp[0:pop_size]
+
+
+# main
+pop_size = 1000
+selected = 1000
+rec_prob = 0.9
+mut_prob = 0.30
+max_gen = 100
+
+# initialize population and store fitness
+pop = initialization(pop_size)
+
+gen = 0
+while gen < max_gen:
+    gen += 1
+
+    # getting mating pool
+    mating_pool = selection(pop, selected)
+
+    # getting children (crosover)
+    children = crossover(mating_pool, rec_prob)
+
+    # applying mutation to children
+    children = mutation(children, mut_prob)
+
+    # replacing worst
+    pop = replace_worst(pop, children)
+
+    print("Average Fitness after Generation " + str(gen), end=" is ")
+    print(format(avfitness(pop), '.10f'))
+
+pop.sort()
+print("Best solution has fitness ", end="")
+print(str(format(pop[0][0], '.10f')))
+print(pop[0][1])

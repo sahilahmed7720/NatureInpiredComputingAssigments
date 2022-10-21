5368453f3a291d05f261600074a6c4c41e651e35
Bharat Chandwani <bharatchandwani1@gmail.com>
2022-10-22 01:29:40 +0530

    Add Rastrigin

---
+++ DE/functions/Rastrigin.py
@@ -0,0 +1,17 @@
+import math
+import numpy as np
+
+from functions.Function import Function
+
+class Rastrigin(Function):
+    def __init__(self, dimension, bounds):
+        Function.__init__(self, dimension, bounds)
+        print('Rastrigin (' + str(dimension) + '-dimensional)')
+        self.dimension = dimension
+        self.bounds = bounds
+    
+    # Evaluate function
+    def eval(self, X):
+        A = 10
+        return A * self.dimension + np.sum([((x*x) - (A * math.cos(2 * math.pi * x))) for x in X])
+    
